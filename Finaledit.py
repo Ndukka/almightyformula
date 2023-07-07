9c2e728f63e15207542402d62b6fea98f31386fe
Ndukka <138584354+Ndukka@users.noreply.github.com>
2023-07-07 13:51:16 +0400

    Update Finaledit.py

--- Finaledit.py
+++ Finaledit.py
@@ -1,34 +1,27 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
-class Code:
-    def __init__(self, A, B, C):
-        self.A = A
-        self.B = B
-        self.C = C
-
-    def ans1(self):
-        return ((-(self.B)) - ((self.B) ** 2 - (4 * (self.A) * (self.C))) ** (1/2)) * (1/(2 * self.A))
-
-    def ans2(self):
-        return ((-(self.B)) + ((self.B) ** 2 - (4 * (self.A) * (self.C))) ** (1/2)) * (1/(2 * self.A))
-
-
+class code:
+        def __init__(self, a, b, c):
+            self.a = a
+            self.b = b
+            self.c = c
+            
+        def ans1(self):
+            return ((-(self.b)) - ((self.b) ** 2 - (4 * (self.a) * (self.c))) ** (1/2)) * (1/(2 * a))
+        #This is just (-b - (b^2 -4ac)**(1/2) * 1/(2*a))
+        def ans2(self):
+            return ((-(self.b)) + ((self.b) ** 2 - (4 * (self.a) * (self.c))) ** (1/2)) * (1/(2 * a))
+            #This is just (-b + (b^2 -4ac)**(1/2) * 1/(2*a))
+            
+print("Nduka's Quadratic Equation Calculator")
 while True:
     try:
-        A = float(input("Enter the prefix of X^2: "))
-        B = float(input("Enter the prefix of X: "))
-        C = float(input("Enter the last number: "))
+        a = float(input("Enter the co-efficient of X^2:"))
+        b = float(input("Enter the co-efficient of X:"))
+        c = float(input("Enter the constant term:"))
         break
     except ValueError:
         print("Invalid input. Please enter a valid number.")
-
-if A == 0:
+if a == 0:
     print("Error: This is not a valid quadratic equation")
 else:
-    quadratic_eq = Code(A, B, C)
-    print("The result of this quadratic equation is:", quadratic_eq.ans1(), "AND", quadratic_eq.ans2())
-
+    print("The result of this quadratic equation is:", code(a, b, c).ans1(), "and", code(a, b, c).ans2())
+    
